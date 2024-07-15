763f5fe6ebf4e63c663f41a14eb096777aaea852
richard kyrk <rkyrk@users.noreply.github.com>
2024-07-15 21:05:01 +0000

    changed version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -12,7 +13,7 @@ st.write(
     """
 )
 
-from snowflake.snowpark.functions import col
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
